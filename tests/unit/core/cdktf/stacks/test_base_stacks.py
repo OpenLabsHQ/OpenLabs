624e1d34b4f387213eae4b6adee5613e12ace5da
Alex Christy <a.christy@ufl.edu>
2025-03-28 22:10:09 -0400

    Add test ensuring that AbstractBaseStack raise NotImplementedError when instantiated

---
+++ tests/unit/core/cdktf/stacks/test_base_stacks.py
@@ -0,0 +1,18 @@
+import pytest
+from cdktf import App
+
+from src.app.core.cdktf.stacks.base_stack import AbstractBaseStack
+from src.app.enums.regions import OpenLabsRegion
+from tests.unit.core.cdktf.config import one_all_template
+
+
+def test_abstract_stack_creation_not_implemented_error() -> None:
+    """Ensure that calling build_resources() from the AbstractBaseStack raises NontImplementedError."""
+    with pytest.raises(NotImplementedError):
+        AbstractBaseStack(
+            scope=App(),
+            template_range=one_all_template,
+            cdktf_id="test-abstract-stack",
+            cdktf_dir="/nonexistent/dir",
+            region=OpenLabsRegion.US_EAST_1,
+        )
