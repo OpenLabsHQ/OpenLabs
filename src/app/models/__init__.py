b88f4fb4e3e2c3e388c02eab6c42687a3d204705
Adamkadaban <adamkadaban@gmail.com>
2025-04-14 15:56:05 -0400

    update models init to import extra models

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -4,10 +4,13 @@ from .range_model import RangeModel
 from .secret_model import SecretModel
 from .template_base_model import OpenLabsUserMixin, OwnableObjectMixin
 from .template_host_model import TemplateHostModel
+from .template_permission_model import TemplatePermissionModel
 from .template_range_model import TemplateRangeModel
 from .template_subnet_model import TemplateSubnetModel
 from .template_vpc_model import TemplateVPCModel
 from .user_model import UserModel
+from .workspace_model import WorkspaceModel
+from .workspace_user_model import WorkspaceUserModel
 
 __all__ = [
     "OpenLabsUserMixin",
@@ -15,8 +18,11 @@ __all__ = [
     "RangeModel",
     "SecretModel",
     "TemplateHostModel",
+    "TemplatePermissionModel",
     "TemplateRangeModel",
     "TemplateSubnetModel",
     "TemplateVPCModel",
     "UserModel",
+    "WorkspaceModel",
+    "WorkspaceUserModel",
 ]
