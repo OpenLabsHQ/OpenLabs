80cff75673c68122e9b1ab6391ad0b8776222090
Alex Christy <a.christy@ufl.edu>
2025-04-16 14:04:34 -0400

    fix: refactor range crud functions to fit new nomeclature

--- src/app/crud/crud_ranges.py
+++ src/app/crud/crud_ranges.py
@@ -1,170 +1,493 @@
 import logging
-import uuid
 
-from sqlalchemy import inspect, select
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import load_only, selectinload
+from sqlalchemy.orm import selectinload
 
-from ..models.range_model import RangeModel
-from ..models.subnet_model import SubnetModel
-from ..models.vpc_model import VPCModel
-from ..schemas.range_schema import RangeID, RangeSchema
-from .crud_vpcs import create_vpc
+from ..models.range_models import BlueprintRangeModel, DeployedRangeModel
+from ..models.subnet_models import BlueprintSubnetModel, DeployedSubnetModel
+from ..models.vpc_models import BlueprintVPCModel, DeployedVPCModel
+from ..schemas.range_schemas import (
+    BlueprintRangeCreateSchema,
+    BlueprintRangeHeaderSchema,
+    BlueprintRangeSchema,
+    DeployedRangeCreateSchema,
+    DeployedRangeHeaderSchema,
+    DeployedRangeSchema,
+)
+from .crud_vpcs import build_blueprint_vpc_models, build_deployed_vpc_models
 
-# Configure logging
 logger = logging.getLogger(__name__)
 
 
-async def get_range_headers(
-    db: AsyncSession, range_id: RangeID, user_id: uuid.UUID | None = None
-) -> list[RangeModel]:
-    """Get list of range headers.
+# ==================== Blueprints =====================
+
+
+async def get_blueprint_range_headers(
+    db: AsyncSession,
+    user_id: int,
+    is_admin: bool = False,
+) -> list[BlueprintRangeHeaderSchema]:
+    """Get list of range blueprint headers.
 
     Args:
     ----
         db (Session): Database connection.
-        range_id (RangeID): ID of the deployed range.
-        user_id (Optional[uuid.UUID]): If provided, only return templates owned by this user.
+        user_id (int): ID of user.
+        is_admin (bool): Admins can see other user's blueprints.
 
     Returns:
     -------
-        Optional[RangeModel]: Range if it exists in database.
+        list[BlueprintRangeHeaderSchema]: List of blueprint range header schemas.
 
     """
-    mapped_range_model = inspect(RangeModel)
-    main_columns = [
-        getattr(RangeModel, attr.key) for attr in mapped_range_model.column_attrs
-    ]
+    stmt = select(
+        BlueprintRangeModel.id,
+        BlueprintRangeModel.name,
+        BlueprintRangeModel.provider,
+        BlueprintRangeModel.vnc,
+        BlueprintRangeModel.vpn,
+    ).select_from(BlueprintRangeModel)
 
-    stmt = (
-        select(RangeModel)
-        .where(RangeModel.id == range_id.id)
-        .options(load_only(*main_columns))
-    )
-
-    if user_id:
-        stmt = stmt.filter(RangeModel.owner_id == user_id)
+    if not is_admin:
+        stmt = stmt.filter(BlueprintRangeModel.owner_id == user_id)
 
     result = await db.execute(stmt)
-    return list(result.scalars().all())
+
+    range_headers = [
+        BlueprintRangeHeaderSchema.model_validate(row_mapping)
+        for row_mapping in result.mappings().all()
+    ]
+
+    logger.info(
+        "Fetched %s range blueprint headers for user: %s.",
+        len(range_headers),
+        user_id,
+    )
+    return range_headers
 
 
-async def get_range(
-    db: AsyncSession, range_id: RangeID, user_id: uuid.UUID | None = None
-) -> RangeModel | None:
-    """Get range by id (uuid).
+async def get_blueprint_range(
+    db: AsyncSession, range_id: int, user_id: int, is_admin: bool = False
+) -> BlueprintRangeSchema | None:
+    """Get range blueprint by ID.
 
     Args:
     ----
         db (Session): Database connection.
-        range_id (RangeID): ID of the range.
-        user_id (Optional[uuid.UUID]): If provided, only return ranges owned by this user.
+        range_id (int): ID of the range blueprint.
+        user_id (int): ID of the user requesting data.
+        is_admin (bool): Admins can see other user's blueprints.
 
     Returns:
     -------
-        Optional[RangeModel]: Range if it exists in database.
+        Optional[BlueprintRangeSchema]: Range blueprint data if it exists in the database.
 
     """
-    # Eagerly fetch relationships to make single query
-    stmt = (
-        select(RangeModel)
-        .options(
-            selectinload(RangeModel.vpcs)
-            .selectinload(VPCModel.subnets)
-            .selectinload(SubnetModel.hosts)
+    options = [
+        selectinload(BlueprintRangeModel.vpcs)
+        .selectinload(BlueprintVPCModel.subnets)
+        .selectinload(BlueprintSubnetModel.hosts),
+    ]
+    range_model = await db.get(BlueprintRangeModel, range_id, options=options)
+
+    if not range_model:
+        logger.info(
+            "Failed to fetch range blueprint: %s. Not found in database!", range_id
         )
-        .filter(RangeModel.id == range_id.id)
+        return None
+
+    if is_admin or range_model.owner_id == user_id:
+        logger.debug("Fetched range blueprint: %s for user %s.", range_id, user_id)
+        return BlueprintRangeSchema.model_validate(range_model)
+
+    logger.warning(
+        "User: %s is not authorized to fetch range blueprint: %s.", user_id, range_id
     )
-
-    if user_id:
-        stmt = stmt.filter(RangeModel.owner_id == user_id)
-
-    result = await db.execute(stmt)
-    return result.scalar_one_or_none()
+    return None
 
 
-async def create_range(
-    db: AsyncSession, range_schema: RangeSchema, owner_id: uuid.UUID
-) -> RangeModel:
-    """Create and add a new range template to the database.
+def build_blueprint_range_models(
+    ranges: list[BlueprintRangeCreateSchema], user_id: int
+) -> list[BlueprintRangeModel]:
+    """Build a list of blueprint range ORM models from creation schemas.
+
+    **Note:** These models will only contain the data available in the
+    schemas (i.e. no database ID).
+
+    Args:
+    ----
+        ranges (list[BlueprintRangeCreateSchema]): Range object data.
+        user_id (int): User who created the subnets.
+
+    Returns:
+    -------
+        list[BlueprintRangeModel]: Corresponding blueprint range models.
+
+    """
+    range_models: list[BlueprintRangeModel] = []
+    for range_schema in ranges:
+        range_model = BlueprintRangeModel(**range_schema.model_dump(), owner_id=user_id)
+        range_model.vpcs = build_blueprint_vpc_models(range_schema.vpcs, user_id)
+        range_models.append(range_model)
+
+    logger.debug(
+        "Build %s blueprint range models for user: %s.", len(range_models), user_id
+    )
+    return range_models
+
+
+async def create_blueprint_range(
+    db: AsyncSession,
+    blueprint: BlueprintRangeCreateSchema,
+    user_id: int,
+) -> BlueprintRangeSchema:
+    """Create and add a new range blueprint to the database session.
+
+    **Note:** This function only adds ranges to the database session. It is the responsibility
+    of the caller to commit the changes to the database or rollback in the event of
+    a failure.
 
     Args:
     ----
         db (Session): Database connection.
-        range_schema (RangeSchema): Range schema pydantic class.
-        owner_id (uuid.UUID): The ID of the user who owns this template.
+        blueprint (BlueprintRangeCreateSchema): Pydantic model of range blueprint data without IDs.
+        user_id (int): User who owns the new range blueprint.
 
     Returns:
     -------
-        OpenLabsRange: The newly created range template.
+        BlueprintRangeSchema: The newly created range blueprint data schema with it's ID.
 
     """
-    range_dict = range_schema.model_dump(exclude={"vpcs"})
+    built_models = build_blueprint_range_models([blueprint], user_id)
 
-    range_dict["owner_id"] = owner_id
+    # Sanity check that we only have a single range model
+    if len(built_models) != 1:
+        msg = f"Built {len(built_models) } range blueprint models from a single schema!"
+        logger.error(msg)
+        raise RuntimeError(msg)
 
-    # Create the Range object (No commit yet)
-    range_obj = RangeModel(**range_dict)
-    db.add(range_obj)  # Stage the range
+    range_model = built_models[0]
 
-    # Build range ID
-    range_id = RangeID(id=range_obj.id)
+    db.add(range_model)
+    logger.debug(
+        "Added range blueprint model with name: %s to database session.",
+        range_model.name,
+    )
 
-    # Create VPCs and associate them with the range (No commit yet)
-    vpc_objects = [
-        await create_vpc(db, vpc_data, owner_id, range_id)
-        for vpc_data in range_schema.vpcs
-    ]
-    db.add_all(vpc_objects)  # Stage VPCs
+    try:
+        await db.flush()
+        await db.refresh(range_model)
+        logger.debug(
+            "Successfully flushed range blueprint: %s owned by user: %s.",
+            range_model.id,
+            user_id,
+        )
+    except Exception as e:
+        logger.exception(
+            "Failed to flush range blueprint to database session for user: %s. Exception: %s.",
+            user_id,
+            e,
+        )
+        raise
 
-    # Commit everything in one transaction
-    await db.commit()
-
-    return range_obj
+    return BlueprintRangeSchema.model_validate(range_model)
 
 
-async def delete_range(db: AsyncSession, range_model: RangeModel) -> bool:
-    """Delete a range entry.
+async def delete_blueprint_range(
+    db: AsyncSession, range_id: int, user_id: int, is_admin: bool = False
+) -> BlueprintRangeSchema | None:
+    """Delete a range blueprint.
+
+    This function only adds delete queries to the database session. It is the responsibility of
+    the caller to commit the changes to the database or rollback in the event of a failure.
 
     Args:
     ----
-        db (AsyncSession): Database session.
-        range_model (RangeModel): Range to delete.
+        db (Sessions): Database connection.
+        range_id (int): ID of the range blueprint.
+        user_id (int): ID of user initiating the delete.
+        is_admin (bool): Admins can delete other user's blueprints.
 
     Returns:
     -------
-        bool: True if successuflly delete. False otherwise.
+        Optional[BlueprintRangeSchema]: Range schema data if it exists in database and was successfully deleted.
 
     """
+    blueprint_range = await get_blueprint_range(db, range_id, user_id, is_admin)
+    if not blueprint_range:
+        logger.warning(
+            "Range blueprint: %s not found for deletion as user: %s. Does user have permissions?",
+            range_id,
+            user_id,
+        )
+        return None
+
+    range_model = BlueprintVPCModel(**blueprint_range.model_dump())
+
+    if not range_model.is_standalone():
+        logger.info(
+            "Failed to delete range blueprint: %s. Not a standalone blueprint!",
+            range_model.id,
+        )
+        return None
+
+    if not is_admin and range_model.owner_id != user_id:
+        logger.warning(
+            "User: %s is not authorized to delete range blueprint: %s.",
+            user_id,
+            range_model.id,
+        )
+        return None
+
     try:
         await db.delete(range_model)
-        await db.commit()
-        return True
+        await db.flush()
+        logger.debug(
+            "Successfully marked range blueprint: %s for deletion.", range_model.id
+        )
     except Exception as e:
-        logger.error("Failed to delete range. Error: %s", e)
-        return False
+        logger.exception(
+            "Failed to mark range blueprint: %s for deletion in database session for user: %s, Exception: %s.",
+            range_model.id,
+            user_id,
+            e,
+        )
+        raise
+
+    return BlueprintRangeSchema.model_validate(range_model)
 
 
-async def is_range_owner(
-    db: AsyncSession, range_id: RangeID, user_id: uuid.UUID
-) -> bool:
-    """Check if a user is the owner of a range template.
+# ==================== Deployed (Instances) =====================
+
+
+async def get_deployed_range_headers(
+    db: AsyncSession,
+    user_id: int,
+    is_admin: bool = False,
+) -> list[DeployedRangeHeaderSchema]:
+    """Get list of deployed range headers.
 
     Args:
     ----
         db (Session): Database connection.
-        range_id (RangeID): ID of the range.
-        user_id (uuid.UUID): ID of the user.
+        user_id (int): ID of user.
+        is_admin (bool): Admins can see other user's blueprints.
 
     Returns:
     -------
-        bool: True if the user is the owner, False otherwise.
+        list[DeployedRangeHeaderSchema]: List of deployed range header schemas.
 
     """
-    stmt = (
-        select(RangeModel)
-        .filter(RangeModel.id == range_id.id)
-        .filter(RangeModel.owner_id == user_id)
-    )
+    stmt = select(
+        DeployedRangeModel.id,
+        DeployedRangeModel.name,
+        DeployedRangeModel.provider,
+        DeployedRangeModel.vnc,
+        DeployedRangeModel.vpn,
+        DeployedRangeModel.description,
+        DeployedRangeModel.date,
+        DeployedRangeModel.state,
+        DeployedRangeModel.region,
+    ).select_from(DeployedRangeModel)
+
+    if not is_admin:
+        stmt = stmt.filter(DeployedRangeModel.owner_id == user_id)
+
     result = await db.execute(stmt)
-    return result.scalar_one_or_none() is not None
+
+    range_headers = [
+        DeployedRangeHeaderSchema.model_validate(row_mapping)
+        for row_mapping in result.mappings().all()
+    ]
+
+    logger.info(
+        "Fetched %s deployed range headers for user: %s.",
+        len(range_headers),
+        user_id,
+    )
+    return range_headers
+
+
+async def get_deployed_range(
+    db: AsyncSession, range_id: int, user_id: int, is_admin: bool = False
+) -> DeployedRangeSchema | None:
+    """Get deployed range by ID.
+
+    Args:
+    ----
+        db (Session): Database connection.
+        range_id (int): ID of the deployed range.
+        user_id (int): ID of the user requesting data.
+        is_admin (bool): Admins can see other user's deployed ranges.
+
+    Returns:
+    -------
+        Optional[DeployedRangeSchema]: Deployed range data if it exists in the database.
+
+    """
+    options = [
+        selectinload(DeployedRangeModel.vpcs)
+        .selectinload(DeployedVPCModel.subnets)
+        .selectinload(DeployedSubnetModel.hosts)
+    ]
+    range_model = await db.get(DeployedRangeModel, range_id, options=options)
+
+    if not range_model:
+        logger.info(
+            "Failed to fetch deployed range: %s. Not found in database!", range_id
+        )
+        return None
+
+    if is_admin or range_model.owner_id == user_id:
+        logger.debug("Fetched deployed range: %s for user %s.", range_id, user_id)
+        return DeployedRangeSchema.model_validate(range_model)
+
+    logger.warning(
+        "User: %s is not authorized to fetch deployed range: %s.", user_id, range_id
+    )
+    return None
+
+
+def build_deployed_range_models(
+    ranges: list[DeployedRangeCreateSchema], user_id: int
+) -> list[DeployedRangeModel]:
+    """Build a list of deployed range ORM models from creation schemas.
+
+    **Note:** These models will only contain the data available in the
+    schemas (i.e. no database ID).
+
+    Args:
+    ----
+        ranges (list[DeployedRangeCreateSchema]): Range object data.
+        user_id (int): User who created the ranges.
+
+    Returns:
+    -------
+        list[DeployedRangeModels]: Corresponding deployed range models.
+
+    """
+    range_models: list[DeployedRangeModel] = []
+    for range_schema in ranges:
+        range_model = DeployedRangeModel(**range_schema.model_dump(), owner_id=user_id)
+        range_model.vpcs = build_deployed_vpc_models(range_schema.vpcs, user_id)
+        range_models.append(range_model)
+
+    logger.debug(
+        "Build %s deployed range models for user: %s.", len(range_models), user_id
+    )
+    return range_models
+
+
+async def create_deployed_range(
+    db: AsyncSession,
+    range_schema: DeployedRangeCreateSchema,
+    user_id: int,
+) -> DeployedRangeSchema:
+    """Create and add a new deployed range to the database session.
+
+    **Note:** This function only adds ranges to the database session. It is the responsibility
+    of the caller to commit the changes to the database or rollback in the event of
+    a failure.
+
+    Args:
+    ----
+        db (Session): Database connection.
+        range_schema (DeployedRangeCreateSchema): Pydantic model of deployed range data without IDs.
+        user_id (int): User who owns the new deployed range.
+
+    Returns:
+    -------
+        DeployedRangeSchema: The newly created deployed range data schema with it's ID.
+
+    """
+    built_models = build_deployed_range_models([range_schema], user_id)
+
+    # Sanity check that we only have a single range model
+    if len(built_models) != 1:
+        msg = f"Built {len(built_models) } deployed range models from a single schema!"
+        logger.error(msg)
+        raise RuntimeError(msg)
+
+    range_model = built_models[0]
+
+    db.add(range_model)
+    logger.debug(
+        "Added deployed range model with name: %s to database session.",
+        range_model.name,
+    )
+
+    try:
+        await db.flush()
+        await db.refresh(range_model)
+        logger.debug(
+            "Successfully flushed deployed range: %s owned by user: %s.",
+            range_model.id,
+            user_id,
+        )
+    except Exception as e:
+        logger.exception(
+            "Failed to flush deployed range to database session for user: %s. Exception: %s.",
+            user_id,
+            e,
+        )
+        raise
+
+    return DeployedRangeSchema.model_validate(range_model)
+
+
+async def delete_deployed_range(
+    db: AsyncSession, range_id: int, user_id: int, is_admin: bool = False
+) -> DeployedRangeSchema | None:
+    """Delete a deployed range.
+
+    This function only adds delete queries to the database session. It is the responsibility of
+    the caller to commit the changes to the database or rollback in the event of a failure.
+
+    Args:
+    ----
+        db (Sessions): Database connection.
+        range_id (int): ID of the deployed range.
+        user_id (int): ID of user initiating the delete.
+        is_admin (bool): Admins can delete other user's deployed ranges.
+
+    Returns:
+    -------
+        Optional[DeployedRangeSchema]: Deployed range schema data if it exists in database and was successfully deleted.
+
+    """
+    deployed_range = await get_deployed_range(db, range_id, user_id, is_admin)
+    if not deployed_range:
+        logger.warning(
+            "Deployed range: %s not found for deletion as user: %s. Does user have permissions?",
+            range_id,
+            user_id,
+        )
+        return None
+
+    range_model = DeployedRangeModel(**deployed_range.model_dump())
+
+    if not is_admin and range_model.owner_id != user_id:
+        logger.warning(
+            "User: %s is not authorized to delete deployed range: %s.",
+            user_id,
+            range_model.id,
+        )
+        return None
+
+    try:
+        await db.delete(range_model)
+        await db.flush()
+        logger.debug(
+            "Successfully marked deployed range: %s for deletion.", range_model.id
+        )
+    except Exception as e:
+        logger.exception(
+            "Failed to mark deployed range: %s for deletion in database session for user: %s, Exception: %s.",
+            range_model.id,
+            user_id,
+            e,
+        )
+        raise
+
+    return DeployedRangeSchema.model_validate(range_model)
