3085fdeef60febe5746350bdc93cd1a292d8225a
Adamkadaban <adamkadaban@gmail.com>
2025-04-20 01:39:19 -0400

    fix incorrect azure spec for vm size

--- src/app/enums/specs.py
+++ src/app/enums/specs.py
@@ -23,9 +23,9 @@ AWS_SPEC_MAP = {
 
 # https://learn.microsoft.com/en-us/azure/virtual-machines/sizes/general-purpose/bv1-series?tabs=sizebasic
 AZURE_SPEC_MAP = {
-    OpenLabsSpec.TINY: "Standard_B1ls2",  # 1 vCPU, 0.5 GiB RAM
+    OpenLabsSpec.TINY: "Standard_B1ls",   # 1 vCPU, 0.5 GiB RAM
     OpenLabsSpec.SMALL: "Standard_B1ms",  # 1 vCPU, 2.0 GiB RAM
     OpenLabsSpec.MEDIUM: "Standard_B2s",  # 2 vCPU, 4.0 GiB RAM
     OpenLabsSpec.LARGE: "Standard_B2ms",  # 2 vCPU, 8.0 GiB RAM
-    OpenLabsSpec.HUGE: "Standard_B4ms",  # 4 vCPU, 16.0 GiB RAM
+    OpenLabsSpec.HUGE: "Standard_B4ms",   # 4 vCPU, 16.0 GiB RAM
 }
