3b523322a508efecf007f3158cd627695c65263d
Alex Christy <a.christy@ufl.edu>
2025-06-04 15:56:50 +0200

    fix(tests): update crud hosts tests to work with the blueprints refactor

--- tests/unit/crud/test_crud_hosts.py
+++ tests/unit/crud/test_crud_hosts.py
@@ -5,10 +5,10 @@ from src.app.crud.crud_hosts import delete_blueprint_host
 from .crud_mocks import DummyBlueprintHost, DummyDB
 
 
-async def test_no_delete_non_standalone_host_templates(
+async def test_no_delete_non_standalone_blueprint_hosts(
     monkeypatch: pytest.MonkeyPatch,
 ) -> None:
-    """Test that delete_host_template() returns false when host template is not standalone."""
+    """Test that attempting to delete a non-standalone blueprint host fails."""
     dummy_db = DummyDB()
 
     # Patch host model is_standalone() method to always return False
@@ -18,7 +18,7 @@ async def test_no_delete_non_standalone_host_templates(
     # Ensure that we get the dummy host from the "db"
     dummy_db.get.return_value = dummy_host
 
-    assert not await delete_blueprint_host(dummy_db, dummy_host, 100)  # type: ignore
+    assert not await delete_blueprint_host(dummy_db, 1, 100)  # type: ignore
 
     # Verify that delete and commit were not called
     dummy_db.delete.assert_not_called()
