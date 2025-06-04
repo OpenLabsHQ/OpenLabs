451decfffada3fdcfb2b59c1380d7c4138cfe047
Alex Christy <a.christy@ufl.edu>
2025-06-04 15:55:24 +0200

    fix(tests): update crud range tests to work with the blueprints refactor

--- tests/unit/crud/test_crud_ranges.py
+++ tests/unit/crud/test_crud_ranges.py
@@ -1,9 +1,28 @@
-import uuid
-from unittest.mock import MagicMock
-
 import pytest
 
-from .crud_mocks import DummyDB, DummyRangeModel
+from src.app.crud.crud_ranges import delete_blueprint_range, delete_deployed_range
+
+from .crud_mocks import DummyBlueprintRange, DummyDB, DummyDeployedRange
+
+
+async def test_no_delete_non_standalone_blueprint_ranges(
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    """Test that deleting non-standalone ranges fails."""
+    dummy_db = DummyDB()
+
+    # Patch host model is_standalone() method to always return False
+    dummy_range = DummyBlueprintRange()
+    monkeypatch.setattr(dummy_range, "is_standalone", lambda: False)
+
+    # Ensure that we get the dummy range from the "db"
+    dummy_db.get.return_value = dummy_range
+
+    assert not await delete_blueprint_range(dummy_db, 1, 100)  # type: ignore
+
+    # Verify that delete and commit were not called
+    dummy_db.delete.assert_not_called()
+    dummy_db.commit.assert_not_called()
 
 
 async def test_delete_range_exception_return_false(
@@ -11,46 +30,19 @@ async def test_delete_range_exception_return_false(
 ) -> None:
     """Test that delete_range() returns False when an exception is raised internally."""
     dummy_db = DummyDB()
-    dummy_range = DummyRangeModel()
+    dummy_range = DummyDeployedRange()
 
     # Force exception to be thrown
-    dummy_db.commit.side_effect = Exception("Forced exception for testing.")
+    exception_msg = "Forced exception for tesitng."
+    dummy_db.flush.side_effect = Exception(exception_msg)
+
+    # Make sure our user owns the range
+    user_id = 1
+    dummy_range.owner_id = user_id
+
+    # Ensure that we get the dummy range from the "db"
+    dummy_db.get.return_value = dummy_range
 
     # Ignoring type mismatches with testing mocks
-    result = await delete_range(dummy_db, dummy_range)  # type: ignore
-
-    assert result is False
-
-
-async def test_is_range_owner_returns_true() -> None:
-    """Test that is_range_owner() returns True when it returns a result with a range owned by the user."""
-    dummy_db = DummyDB()
-
-    # Create a dummy result with a non-None value returned by scalar_one_or_none()
-    dummy_result = MagicMock()
-    dummy_result.scalar_one_or_none.return_value = object()
-    dummy_db.execute.return_value = dummy_result
-
-    dummy_range_id = RangeID(id=uuid.uuid4())
-    dummy_user_id = uuid.uuid4()
-
-    # Ignoring type mismatches with testing mocks
-    result = await is_range_owner(dummy_db, dummy_range_id, dummy_user_id)  # type: ignore
-
-    assert result is True
-
-
-async def test_is_range_owner_returns_false() -> None:
-    """Test that the is_range_owner() return False when it returns no results of a range owned the user."""
-    dummy_db = DummyDB()
-    # Create a dummy result with None returned by scalar_one_or_none() to simulate no result
-    dummy_result = MagicMock()
-    dummy_result.scalar_one_or_none.return_value = None
-    dummy_db.execute.return_value = dummy_result
-
-    dummy_range_id = RangeID(id=uuid.uuid4())
-    dummy_user_id = uuid.uuid4()
-
-    # Ignoring type mismatches with testing mocks
-    result = await is_range_owner(dummy_db, dummy_range_id, dummy_user_id)  # type: ignore
-    assert result is False
+    with pytest.raises(Exception, match=exception_msg):
+        await delete_deployed_range(dummy_db, range_id=1, user_id=user_id)  # type: ignore
