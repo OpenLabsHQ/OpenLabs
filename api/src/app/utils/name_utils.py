8d4b5033bf19b27c75a59ae5428faf3b0ad9651d
Alex Christy <a.christy@ufl.edu>
2025-07-24 12:59:28 +0200

    fix(api): update normalization function for names to create kebab case sames for stronger string safety

--- api/src/app/utils/name_utils.py
+++ api/src/app/utils/name_utils.py
@@ -1,3 +1,27 @@
+import re
+
+
 def normalize_name(name: str) -> str:
-    """Remove problematic characters from user-supplied names."""
-    return name.strip().replace(" ", "")
+    """Remove problematic characters from user-supplied names for cloud deployments while maintaining readability.
+
+    Args:
+        name: Name to normalize.
+
+    Returns:
+        Name normalized to a safe kebab case version.
+
+    """
+    normalized_name = name.lower()
+
+    # Strip out disallowed characters
+    normalized_name = re.sub(r"[^a-z0-9\-]", "-", normalized_name)
+
+    # Remove extra hyphens
+    normalized_name = re.sub(r"-+", "-", normalized_name)
+    normalized_name = normalized_name.strip("-")
+
+    if not normalized_name:
+        msg = f"Name is empty after normalization. Original name: '{name}'"
+        raise ValueError(msg)
+
+    return normalized_name
